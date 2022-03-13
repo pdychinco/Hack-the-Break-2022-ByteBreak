09a0cfc9081e54beb5193ede92e3de53a48b6c6e
Izabelle Datayan <i_datayan@yahoo.com>
2022-03-13 03:36:37 -0700

    added a settings.html

---
+++ scripts/accountsettings.js
@@ -0,0 +1,9 @@
+document.getElementById('submit').addEventListener("click", function() {
+    alert("Notification Preference updated");
+})
+
+// if (document.getElementById('text').checked && document.getElementById('submit').addEventListener("click") {
+//     alert("checked");
+// } else {
+//     alert("You didn't check it! Let me check it for you.");
+// })
